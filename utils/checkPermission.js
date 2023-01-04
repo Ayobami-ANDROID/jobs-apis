a8fd697bd37a35b19619f9da7bb3bbbc92e28cb0
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2023-01-04 14:53:28 +0100

    jobs

---
+++ utils/checkPermission.js
@@ -0,0 +1,8 @@
+const {UnauthenticatedError} = require('../errors/index')
+const checkPermissions = (requestUser, resourceUserId) => {
+    if (requestUser.userId === resourceUserId.toString()) return
+  
+    throw new UnAuthenticatedError('Not authorized to access this route')
+  }
+  
+  module.exports = checkPermissions
